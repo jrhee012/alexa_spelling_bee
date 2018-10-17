097c1a78b2e41db86713de70a7a6b557a97e2713
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-10-17 09:36:42 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const Alexa = require('ask-sdk');
+
+const msg = {
+    exit: 'Goodbye!',
+    fallback: 'Hm... Can you say that again?',
+    help: 'Help!',
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+const HelpHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest'
+            && request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speech = msg.help;
+        return handlerInput.responseBuilder
+            .speak(speech)
+            .reprompt(speech)
+            .getResponse();
+    },
+};
+
+const FallbackHandler = {
+    // 2018-Aug-01: AMAZON.FallbackIntent is only currently available in en-* locales.
+    //              This handler will not be triggered except in those locales, so it can be
+    //              safely deployed for any locale.
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest'
+            && request.intent.name === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speech = msg.fallback;
+        return handlerInput.responseBuilder
+            .speak(speech)
+            .reprompt(speech)
+            .getResponse();
+    },
+};
+
+const ExitHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest'
+            && (request.intent.name === 'AMAZON.CancelIntent'
+                || request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak(msg.exit)
+            .getResponse();
+    },
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+        return handlerInput.responseBuilder.getResponse();
+    },
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
+        console.log(`Error stack: ${error.stack}`);
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        return handlerInput.responseBuilder
+            .speak(requestAttributes.t('ERROR_MESSAGE'))
+            .reprompt(requestAttributes.t('ERROR_MESSAGE'))
+            .getResponse();
+    },
+};
+
+exports.handler = skillBuilder
+    .addRequestHandlers(
+        GetNewFactHandler,
+        HelpHandler,
+        ExitHandler,
+        FallbackHandler,
+        SessionEndedRequestHandler,
+    )
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
